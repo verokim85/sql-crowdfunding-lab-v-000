982c7a6dda4eedbf660ba7f8fca0eb11a9876dd1
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:38:03 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, SUM(pledges.amount)
 FROM pledges GROUP BY projects
-JOIN  projects ON projects.id = pledges.project_id
+JOIN projects ON projects.id = pledges.project_id
 ORDER BY projects.title, pledges.amount ASC"
 end
 
