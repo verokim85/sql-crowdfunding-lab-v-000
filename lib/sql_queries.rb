a17708bd5bf434e51c3be395bedb2a6b0d8a3c9f
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:43:39 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM pledges
 JOIN projects ON projects.id = pledges.project_id
 GROUP BY projects.title
-
+WHERE pledges.amount > projects.funding_goal  
 "
 end
 
