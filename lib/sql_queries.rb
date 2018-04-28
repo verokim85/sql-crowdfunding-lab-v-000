bd8a8b91e694eb451f1d83cb9ec74b40a1ecd17d
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:54:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,11 +23,11 @@ ORDER BY users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.amount, SUM(pledges.amount)
+"SELECT projects.title, SUM(pledges.amount)
 FROM pledges
 JOIN projects ON projects.id = pledges.project_id
 GROUP BY projects.title
-HAVING (SUM(pledges.amount) - projects.amount) > projects.amount"
+HAVING (SUM(pledges.amount) - projects.fundgin_goal) > projects.amount"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
