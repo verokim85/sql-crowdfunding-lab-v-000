eca009605be38fc3d38437186db0e5c95fe22d8f
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:12:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, sum(pledges.amount)
 from pledges
-group by users
+group by users"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
