17c9cb1be328ab97f115485abd19436fe010ddc2
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:57:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ HAVING SUM(pledges.amount) - projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, users.age, SUM(pledges.amount)
+  "SELECT users.name, SUM(pledges.amount)
   FROM pledges
   JOIN users ON users.id = pledges.user_id
   GROUP BY users.name
