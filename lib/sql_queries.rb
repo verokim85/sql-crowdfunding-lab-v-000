c490c973b8c0fff49c3f6d6e28a316774fcaa915
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:08:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, sum(pledges.amount) group by users
-FROM users
+from users
 INNER JOIN pledges
 ON users.id = pledges.user_id
 ORDER BY users.name ASC"
