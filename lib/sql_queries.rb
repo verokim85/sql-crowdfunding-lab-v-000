09300249ad4d782a20a609d0d15ab5e8faa7a049
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 23:30:27 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,8 @@ ORDER BY projects.title, pledges.amount ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-
+"SELECT users.name, users.age FROM users, pledge.amount FROM pledges 
+"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
