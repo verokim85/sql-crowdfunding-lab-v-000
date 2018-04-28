58f335994e3d714b8489de81c65c59aa492d21a4
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 22:47:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 FROM pledges
 INNER JOIN projects
 ON projects.id = pledges.id
-ORDER BY projects.title ASC"
+ORDER BY projects.title, pledges.amount ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
