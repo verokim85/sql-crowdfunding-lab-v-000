b1343dfe588d8086759181cf1c82593450ff59b6
Veronica Kim <vero.a.kim@gmail.com>
2018-04-28 22:33:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount
 FROM projects
-INNER JOIN pledges
+INNER JOIN pledges 
 ON projects.id = pledges.id
 ORDER BY projects.title ASC"
 end
